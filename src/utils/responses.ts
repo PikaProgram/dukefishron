c9e269359c916ed988adada7ee1909106e8e5951
PikaProgram <rasyad.satyatma01@gmail.com>
2020-08-09 22:33:00 +0700

    forgot some stuff :p

---
+++ src/utils/responses.ts
@@ -0,0 +1,47 @@
+/* eslint-disable quotes */
+/**
+ * Pick The Responses That Match The Command's Category.
+ *
+ * If You Want To Add More Responses, Create An Issue.
+ *
+ * Requirements:
+ * 1. Responses That Are Terraria Themed Are More Likely To Be Added.
+ * 2. Must Be SFW.
+ * 3. Take Examples From Existing Responses.
+ */
+
+export const awaitingMessages: string[] = [
+  // Event Messages
+  'The Blood Moon is rising...',
+  'Slime is falling from the sky!',
+  "Party time's over!",
+  "The old one's army is approaching!",
+  'A solar eclipse is happening!',
+  'A goblin army has arrived!',
+  'The Frost Legion has arrived!',
+  'The pirates have arrived!',
+  'Martians are invading!',
+  'Celestial creatures are invading!',
+  'The spirit of Christmas spreads cheer...',
+  'The spirit of Halloween penetrates the air...',
+];
+
+export const ownerOnly = [
+  'You Are Not My Master',
+  'Only My Master Can Do That',
+  "Do You Think You're My Master?",
+  'Defeat My Master, And Then Come Back',
+];
+
+export const userMissingPerms = [
+  'You Are Too Weak',
+  'A Bad Idea To Use That',
+  "Stop, Don't Even Try",
+  'Train For Another 1000 Years, Then Try Again',
+];
+
+export const botMissingPerms = [
+  'I Am Too Weak To Do That',
+  "I'm Afraid I Cannot Do That",
+  "Are You Joking? There's No Way I Can Do That",
+];
